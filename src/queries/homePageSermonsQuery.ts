6293263ede418ece27e2a56caf63499c5cb5863f
Marcos Ramos <marcosperes.contato@gmail.com>
2025-05-23 21:25:08 -0300

    added id on homepageSermonQuery

--- src/queries/homePageSermonsQuery.ts
+++ src/queries/homePageSermonsQuery.ts
@@ -2,6 +2,7 @@ import groq from "groq";
 
   export const homePageSermonsQuery = groq`
 *[_type == "sermonSummary"] | order(date desc)[0...5] {
+      _id,
       title,
       date,
       "slug": slug,
